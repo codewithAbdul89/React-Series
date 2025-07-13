57c9219960547db53e6b6169d6ab84899929dddd
Abdul Rehman <muabdulrehman58@gmail.com>
2025-07-13 22:27:03 +0500

    Auto Uploader

--- 02viteCustomReact/src/App.jsx
+++ 02viteCustomReact/src/App.jsx
@@ -10,41 +10,47 @@ import { createRoot } from 'react-dom/client'
 
 
 function App() {
-  let [count,countValue]=useState(5)
-  function AddValue(){
-    if(count===10){
+  let [count, countValue] = useState(5)
+  function AddValue() {
+    if (count === 100) {
       countValue(10)
     }
-    else{
-count=count+1
-countValue(count)
+    else {
+      
+    
+      countValue(counter=> counter+ 1)
+      countValue(counter=> counter+ 1)
+      countValue(counter=> counter+ 1)
+      countValue(counter=> counter+ 1)
+      countValue(counter=> counter+ 1)
+
     }
 
 
-  
-}
-
-const RemoveValue=()=>{
-  if(count===0){
-    countValue(0)
-  }
-  else{
-countValue(count-1)
 
   }
-}
+
+  const RemoveValue = () => {
+    if (count === 0) {
+      countValue(0)
+    }
+    else {
+      countValue(count - 1)
+
+    }
+  }
 
 
   return (
-<>
-<h1>Abdul and chai!</h1>
-<h1>Counter Value : {count}</h1>
+    <>
+      <h1>Abdul and chai!</h1>
+      <h1>Counter Value : {count}</h1>
 
-<button onClick={AddValue}>Add Value</button>
-<br />
-<button onClick={RemoveValue}>Remove Value</button>
+      <button onClick={AddValue}>Add Value</button>
+      <br />
+      <button onClick={RemoveValue}>Remove Value</button>
 
-</>
+    </>
   )
 }
 
