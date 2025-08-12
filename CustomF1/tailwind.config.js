fcc9d3c2b4100c3a21d48c74b4a16e450730e4b9
Abdul Rehman <muabdulrehman58@gmail.com>
2025-08-12 18:38:06 +0500

    Simple Html

---
+++ CustomF1/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    "./index.html",
+    "./**/*.html",
+    "./folder/**/*.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
